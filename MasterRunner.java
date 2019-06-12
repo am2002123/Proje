c2eb9fdaf3d145d60c55c3cb5f0ad777f65fba55
am2002123 <50952191+am2002123@users.noreply.github.com>
2019-06-12 17:57:06 -0400

    Update MasterRunner.java

--- MasterRunner.java
+++ MasterRunner.java
@@ -1,1 +1,60 @@
 
+public class Quotient implements InterfacePS{
+
+	float d = 0;
+
+
+	public void quotient(float a, float b){
+
+
+		d=a/b;
+
+
+
+		d = (float)Math.round(d*1000)/(float)1000;
+
+
+
+
+		System.out.printf("The quotient is %.3f \n", d);
+
+	}
+
+
+
+	public static void main (String[]args){
+
+		Quotient q  = new Quotient();
+		q.quotient(24,7);
+	}
+
+
+}
+
+public class RunnerPerf implements Interfacejava{
+
+
+	public boolean perfS(int num){
+
+
+	double sr = Math.sqrt(num);
+
+
+        return ((sr - Math.floor(sr)) == 0);
+
+
+}
+
+
+	public static void main(String[] args){
+
+	RunnerPerf r= new RunnerPerf();
+	System.out.print(r.perfS(4));
+
+
+		}
+
+
+
+	}
+// I (Annie) did the perfect square and if the square root of  the number was an whole number I made the method return true
